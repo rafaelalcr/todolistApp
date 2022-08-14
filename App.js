9188eae7cfac07bc6c313f33184006a6ff345fcc
Rafaela <rafaela99fs@hotmail.com>
2022-08-14 18:58:17 +0100

    feat(App): reponsible to show the header and the list of items

--- App.js
+++ App.js
@@ -1,11 +1,60 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  View,
+  Text,
+  FlatList,
+  TouchableOpacity,
+} from "react-native";
+import { AntDesign } from "@expo/vector-icons";
+import AddModal from "./components/AddModal";
+import ListItems from "./components/ListItems";
 
 export default function App() {
+  const [listItems, setListItems] = useState([]);
+  const [modalVisible, setModalVisible] = useState(false);
+
+  const openModal = () => {
+    setModalVisible(true);
+  };
+
+  const closeModal = (visible) => {
+    setModalVisible(visible);
+  };
+
+  const addItemHandler = (item) => {
+    if (item.length === 0) {
+      return;
+    }
+    setListItems((currentItems) => [
+      ...currentItems,
+      { id: Math.random().toString(), value: item },
+    ]);
+    setModalVisible(false);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.header}>
+        <Text style={styles.text}>ToDo List</Text>
+        <TouchableOpacity onPress={openModal}>
+          <AntDesign name="pluscircle" size={30} color="#A35BDA" />
+        </TouchableOpacity>
+      </View>
+      {modalVisible && (
+        <AddModal
+          visible={modalVisible}
+          onAddItem={addItemHandler}
+          onClose={closeModal}
+        />
+      )}
+      <FlatList
+        keyExtractor={(item, index) => item.id}
+        data={listItems}
+        renderItem={(itemData) => (
+          <ListItems id={itemData.item.id} text={itemData.item.value} />
+        )}
+      />
     </View>
   );
 }
@@ -13,8 +62,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+  },
+  header: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "flex-start",
+    marginVertical: 50,
+  },
+  text: {
+    fontSize: 30,
+    fontWeight: "bold",
+    width: 300,
   },
 });
