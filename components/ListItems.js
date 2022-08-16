6a8b31739cdab7e934a7b747d039230d3bec15c0
Rafaela <rafaela99fs@hotmail.com>
2022-08-16 22:54:28 +0100

    feat(ListItems): it was passed props for the edit and delete options

--- components/ListItems.js
+++ components/ListItems.js
@@ -1,12 +1,32 @@
 import React, { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import { RoundedCheckbox } from "react-native-rounded-checkbox";
+import ChangeModal from "./ChangeModal";
 
 const ListItems = (props) => {
   const [isSelected, setIsSelected] = useState(false);
+  const [modalVisible, setModalVisible] = useState(false);
 
   const decorationTextHandler = () => {
     setIsSelected((current) => !current);
+  };
+
+  const openModal = () => {
+    setModalVisible(true);
+  };
+
+  const closeModal = (visible) => {
+    setModalVisible(visible);
+  };
+
+  const editItemHandler = (item) => {
+    props.editItem(props.id, item);
+    setModalVisible(false);
+  };
+
+  const deleteItemHandler = () => {
+    props.deleteItem(props.id);
+    setModalVisible(false);
   };
 
   return (
@@ -22,9 +42,19 @@ const ListItems = (props) => {
           styles.label,
           { textDecorationLine: isSelected ? "line-through" : "none" },
         ]}
+        onPress={openModal}
       >
         {props.text}
       </Text>
+      {modalVisible && (
+        <ChangeModal
+          id={props.id}
+          text={props.text}
+          onEditItem={editItemHandler}
+          onDeleteItem={deleteItemHandler}
+          onClose={closeModal}
+        />
+      )}
     </View>
   );
 };
